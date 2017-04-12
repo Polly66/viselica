f43f495e03a7a26595e4db6cb826612c53a85405
Polly66 <galago66@gmail.com>
2017-04-12 20:51:37 +0500

    add comments

--- viselica2.js
+++ viselica2.js
@@ -1,3 +1,16 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var wordSource = [
+    new CreateWordStock("насекомые", ["комар", "муха", "гусеница", "стрекоза", "червяк"] ),
+    new CreateWordStock("фрукты", ["ананас", "дыня", "гранат", "айва", "апельсин"] )
+];
+var guessTheme;
+var guessWord = pickWord(wordSource);
+
+var remainingLetters = guessWord.length;
+var attemps = 11;
+var missArray = [];
+
 $(function () {
     $(".start-game").click(function (){
         $(".game").css("display","block");
@@ -11,99 +24,118 @@ $(function () {
     });
 });
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
 createGamePlace();
-
-var wordSource = [];
-var source1 = new CreateWordStock("насекомые", ["комар", "муха", "гусеница", "стрекоза", "червяк"] );
-var source2 = new CreateWordStock("фрукты", ["ананас", "дыня", "гранат", "айва", "апельсин"] );
-
-wordSource[0] = source1;
-wordSource[1] = source2;
-
-var guessTheme;
-var guessWord = pickWord(wordSource);
-$(".themes").html('<strong>Тема:</strong> ' + guessTheme);
-
-setupWordTable(guessWord);
-
-function setupWordTable( guessWord ) {
+setupWordTable();
+/**
+ * Отрисовка табло и темы
+ */
+function setupWordTable() {
+    $(".themes").html('<strong>Тема:</strong> ' + guessTheme);
     for (var i = 0; i < guessWord.length; i++) {
         $(".guessWordTable").append('<div class="guessLetterTable"></div>');
     }
 }
-
-var remainingLetters = guessWord.length;
-var attemps = 11;
-var missArray = [];
-
-
+/**
+ * Отображает ход игры
+ * @param guess текущая загаданная буква
+ * @returns {boolean} возвращает false при нажатии кнопки "Отмена" диалогового окна или отгадывания буквы
+ */
 function gameStep(guess) {
     msg = "Введите, пожалуйста, букву или загаданное слово целиком. Для выхода из игры нажмите Отмена";
+    var openedCharsCount; //Кол-во открытых букв в текущий ход
 
-    if (remainingLetters > 0 && attemps > 0) {
-        showPlayerProgress(attemps);
+    if (!remainingLetters || !attemps) {
+        bootbox.alert( "Было загадано слово - " + guessWord);
+        return;
+    }
 
-        if ( guess !== null ) {
-            var lowerGuess = guess.toLowerCase();
-        } else {
+    if ( guess !== null ) {
+        guess = guess.toLowerCase();
+    } else {
+        $('.bootbox.modal').modal('hide');
+        bootbox.alert("Пока!");
+        return false;
+    }
+
+    showPlayerProgress(attemps);
+
+
+    if (guess.length !== 1) {
+        if (guess === guessWord) {
             $('.bootbox.modal').modal('hide');
-            bootbox.alert("Пока!");
+            bootbox.alert("Поздравляем!");
+            updateGameState(guess);
             return false;
+        } else {
+            msg = "Пожалуйста, введите одну букву либо слово целиком";
+            attemps--;
+            drawMen(attemps);
+            showPlayerProgress(attemps);
         }
+    } else {
+        if (isRepeat(guess)) {
+            msg = "Такую букву уже проверяли! Введите другую";
+        }
+        else {
+            missArray.push(guess);
+            openedCharsCount = updateGameState(guess);
+            remainingLetters -= openedCharsCount;
 
-        if (lowerGuess.length !== 1) {
-            if (lowerGuess === guessWord) {
+            if (!remainingLetters) {
                 $('.bootbox.modal').modal('hide');
                 bootbox.alert("Поздравляем!");
+                updateGameState(guess);
                 return false;
-            } else {
-                msg = "Пожалуйста, введите одну букву либо слово целиком";
+            }
+
+            if (openedCharsCount === 0) {
                 attemps--;
                 drawMen(attemps);
                 showPlayerProgress(attemps);
-            }
-        } else {
-            remainingLetters -= updateGameState(lowerGuess, guessWord);
-
-            if (updateGameState(lowerGuess, guessWord) === 0 && !isRepeat(lowerGuess)) {
-                attemps--;
-                drawMen(attemps);
-                showPlayerProgress(attemps);
-                $(".missingLettersShow").append('<span>' + lowerGuess + ', <span>');
-                missArray.push(lowerGuess);
+                $(".missingLettersShow").append('<span>' + guess + ', <span>');
             } else {
-                msg = "Такую букву уже проверяли! Введите другую";
+                msg = "Буква '" + guess + "' есть в слове! Загадывайте следующую букву";
             }
-
         }
-
-        bootbox.prompt(
-            msg,
-            gameStep
-        );
-    } else {
-        bootbox.alert( "Было загадано слово - " + guessWord);
     }
+
+    bootbox.prompt(
+        msg,
+        gameStep
+    );
 }
 
-
+/**
+ * Конструктор хранилища слов
+ * @param theme тема загаданного слова
+ * @param words массив со словами для загадывания
+ */
 function CreateWordStock (theme, words) {
     this.theme = theme;
     this.words = words;
-};
-
-function pickWord (arr) {
-    var guessObj = arr[ Math.floor( Math.random() * arr.length ) ];
-    guessTheme = guessObj.theme;
-    return guessObj.words[ Math.floor( Math.random() * arr.length ) ];
 }
 
+/**
+ * Отрисовка человечка
+ * @param attemps количество оставшихся попыток
+ */
+function pickWord (wordSource) {
+    var guessObj = wordSource[ Math.floor( Math.random() * wordSource.length ) ];
+    guessTheme = guessObj.theme;
+    return guessObj.words[ Math.floor( Math.random() * guessObj.words.length ) ];
+}
+
+/**
+ * Отрисовка счетчика попыток
+ * @param attemps количество оставшихся попыток
+ */
 function showPlayerProgress(attemps) {
     $("#attempsCounter").html('<strong>Осталось попыток:</strong> ' + attemps);
 }
 
+/**
+ * Отрисовка первоначального поля с виселицей
+ */
 function createGamePlace () {
     ctx.strokeStyle = "Red";
     ctx.lineWidth = 4;
@@ -117,8 +149,12 @@ function createGamePlace () {
     ctx.strokeStyle = "Black";
 }
 
-function drawMen (a) {
-    switch(a) {
+/**
+ * Отрисовка человечка
+ * @param attemps количество оставшихся попыток
+ */
+function drawMen (attemps) {
+    switch(attemps) {
         case 10:
             ctx.lineWidth = 4;
             ctx.beginPath();
@@ -191,6 +227,13 @@ function drawMen (a) {
     }
 }
 
+/**
+ * Отрисовка круга
+ * @param x координата центра окружности по оси Ox
+ * @param y координата центра окружности по оси Oy
+ * @param radius радиус окружности
+ * @param fillCircle булево значение: true - не заливать окружность цветом, false - нет
+ */
 function circle (x, y, radius, fillCircle)  {
     if (fillCircle) {
         ctx.beginPath();
@@ -202,20 +245,33 @@ function circle (x, y, radius, fillCircle)  {
     ctx.stroke();
 }
 
-function updateGameState(lowerGuess, guessWord) {
+/**
+ * Возвращает число открытых букв и отрисовывает табло
+ * @param guess текущая загаданная буква
+ * @returns {number} число открытых букв в слове
+ */
+function updateGameState(guess) {
     var a = 0;
     for (var j = 0; j < guessWord.length; j++) {
-        if (guessWord[j] === lowerGuess) {
-            $('.guessWordTable').children().eq(j).html(lowerGuess);
+        if (guessWord[j] === guess) {
+            $('.guessWordTable').children().eq(j).html(guess);
             a++;
+        }
+        else if (guess.length == guessWord.length && guessWord[j] == guess[j]) {
+            $('.guessWordTable').children().eq(j).html(guess[j]);
         }
     }
     return a;
 }
 
-function isRepeat (a) {
+/**
+ * Проверяет на повторный ввод буквы
+ * @param guess текущая загаданная буква
+ * @returns {boolean} возвращает true в случае ввода ранее введенной буквы
+ */
+function isRepeat (guess) {
     for (var i = 0, len = missArray.length; i < len; i++) {
-        if ( missArray[i] === a ) {
+        if ( missArray[i] === guess ) {
             return true;
         }
     }
